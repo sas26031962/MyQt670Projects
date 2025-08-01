97595164ec4b84ba768d590f9e3da554d0acd58d
sas26031962 <sasx26031962@gmail.com>
2025-08-02 01:44:41 +0300

    Рефакторинг main.cpp, добавление .gitignore

--- ConApp1/main.cpp
+++ ConApp1/main.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char *argv[])
 {
 
-    std::string s = std::format("Hello, {}!", "world"); // "Hello, world!"
+    std::string Name = "world";
+    std::string s = std::format("Hello, {}!", Name); // "Hello, world!"
 
     std::cout << s;
 
